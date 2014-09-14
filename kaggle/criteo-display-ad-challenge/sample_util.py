71f1c35c38a9cc70fc9892cd9e2a6239a0915c6c
jordanatlas <jordan.atlas@gmail.com>
2014-09-14 12:43:11 -0700

    forgot this file in last commit

--- kaggle/criteo-display-ad-challenge/sample_util.py
+++ kaggle/criteo-display-ad-challenge/sample_util.py
@@ -25,7 +25,8 @@ def generate_sample(fname, fsize, count_desired, outfname, has_header=True):
   count_added = 0
   for i, l in enumerate(open(fname)):
     if i == 0 and has_header:
-	  result.append(l)	
+      result.append(l)	
+      print("Added header row with", l)
       continue
     elif random.random() < p_select:
       result.append(l)
